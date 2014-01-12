86bed4852aac0627c79d5ad52f295e7b9ad273d6
Mike McNeil <michael.r.mcneil@gmail.com>
2014-01-12 00:33:01 -0600

    Update sailsrc.

--- json/sailsrc.js
+++ json/sailsrc.js
@@ -1,34 +1,34 @@
 /**
  * .sailsrc
- * 
+ *
  * @param  {[type]} scope [description]
  * @return {[type]}       [description]
  */
-module.exports = function (scope) {
+module.exports = function(scope) {
 
 	return {
-		'generators': {
-			'options': {
-				'engine': 'ejs',
-				'linker': true,
-				'adapter': 'sails-disk'
+		generators: {
+			options: {
+				engine: 'ejs',
+				linker: true,
+				adapter: 'sails-disk'
 			},
-			'modules': {
-				'new': 'sails-generate-new',
-				'backend': 'sails-generate-backend',
-				'frontend': 'sails-generate-frontend',
-				'gitigore': 'sails-generate-gitigore',
-				'packagejson': 'sails-generate-packagejson',
-				'readme': 'sails-generate-readme',
-				'model': 'sails-generate-model',
-				'controller': 'sails-generate-controller',
-				'api': 'sails-generate-api',
-				'policy': 'sails-generate-policy',
-				'response': 'sails-generate-response',
-				'view': 'sails-generate-view',
-				'adapter': 'sails-generate-adapter',
-				'generator': 'sails-generate-generator',
-				'hook': 'sails-generate-hook'
+			modules: {
+				new: 'sails-generate-new',
+				backend: 'sails-generate-backend',
+				frontend: 'sails-generate-frontend',
+				gruntfile: 'sails-generate-gruntfile',
+				model: 'sails-generate-model',
+				controller: 'sails-generate-controller',
+				api: 'sails-generate-api',
+				policy: 'sails-generate-policy',
+				service: 'sails-generate-service',
+				apiresponse: 'sails-generate-apiresponse',
+				blueprints: 'sails-generate-blueprints',
+				view: 'sails-generate-view',
+				adapter: 'sails-generate-adapter',
+				generator: 'sails-generate-generator',
+				hook: 'sails-generate-hook'
 			}
 		}
 	};
