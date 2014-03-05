f940d5c690d0c060b3cb6cfefe48811fe927fe36
Mike McNeil <michael.r.mcneil@gmail.com>
2014-03-04 19:47:13 -0600

    Fix try/catch blocks.

--- templates/app.js
+++ templates/app.js
@@ -36,17 +36,18 @@ try {
 var rc;
 try {
 	rc = require('rc');
-} catch (e) {}
-try {
-	rc = require('sails/node_modules/rc');
-} catch (e) {}
-if (!rc) {
-	console.error('Could not find dependency: `rc`.');
-	console.error('Your `.sailsrc` file(s) will be ignored.');
-	console.error('To resolve this, run:');
-	console.error('npm install rc --save');
-	rc = function () { return {}; };
+} catch (e0) {
+	try {
+		rc = require('sails/node_modules/rc');
+	} catch (e1) {
+		console.error('Could not find dependency: `rc`.');
+		console.error('Your `.sailsrc` file(s) will be ignored.');
+		console.error('To resolve this, run:');
+		console.error('npm install rc --save');
+		rc = function () { return {}; };
+	}
 }
+
 
 // Start server
 sails.lift(rc('sails'));
