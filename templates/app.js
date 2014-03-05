85d02b874c79cadca7816382fb18a1f971b377a4
Mike McNeil <michael.r.mcneil@gmail.com>
2014-03-04 19:25:15 -0600

    Made app.js 'try' harder to find a working 'rc' dep.

--- templates/app.js
+++ templates/app.js
@@ -19,12 +19,10 @@
  */
 
 // Ensure a "sails" can be located:
-var sails, rc;
+var sails;
 try {
 	sails = require('sails');
-	rc = require('rc');
-}
-catch (e) {
+} catch (e) {
 	console.error('To run an app using `node app.js`, you usually need to have a version of `sails` installed in the same directory as your app.');
 	console.error('To do that, run `npm install sails`');
 	console.error('');
@@ -32,6 +30,22 @@ catch (e) {
 	console.error('When you run `sails lift`, your app will still use a local `./node_modules/sails` dependency if it exists,');
 	console.error('but if it doesn\'t, the app will run with the global sails instead!');
 	return;
+}
+
+// Try to get `rc` dependency
+var rc;
+try {
+	rc = require('rc');
+} catch (e) {}
+try {
+	rc = require('sails/node_modules/rc');
+} catch (e) {}
+if (!rc) {
+	console.error('Could not find dependency: `rc`.');
+	console.error('Your `.sailsrc` file(s) will be ignored.');
+	console.error('To resolve this, run:');
+	console.error('npm install rc --save');
+	rc = function () { return {}; };
 }
 
 // Start server
